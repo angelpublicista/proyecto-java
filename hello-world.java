5dc0f8c196dd8ddb2b41105dd5fcc796d4367ec3
angelpublicista <angelpublicista.1@gmail.com>
2024-09-08 00:52:39 -0500

    feat: change message

--- hello-world.java
+++ hello-world.java
@@ -2,6 +2,6 @@ class HelloWorld {
     public static void main(String[] args){
         int i=20;
         
-        System.println("Hello world");
+        System.println("Hello world, Christian is very ugly");
     }
 }
